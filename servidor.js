a95a9661105ce2c59d058d162da65c6b69bd6f61
Lizeth <lizeth.vazquez@cua.uam.mx>
2026-03-10 12:41:48 -0600

    node

---
+++ servidor.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const mysql = require('mysql2');
+const app = express();
+
+// 1. Configuración de la conexión
+const db = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'root', // Tu contraseña
+  database: 'mydb' // <--- ¡PON AQUÍ EL NOMBRE DE TU BD!
+});
+
+// 2. Conectar a la base de datos
+db.connect(err => {
+  if (err) {
+    console.error('Error conectando a MySQL:', err.message);
+    return;
+  }
+  console.log('¡Conexión exitosa a la base de datos!');
+});
+
+// Ruta para obtener todos los libros
+app.get('/libros', (req, res) => {
+  const query = 'SELECT * FROM libros';
+  
+  db.query(query, (err, results) => {
+    if (err) {
+      return res.status(500).send('Error al consultar la base de datos');
+    }
+    res.json(results); // Esto enviará los libros en formato JSON al navegador
+  });
+});
+
+// 3. Levantar el servidor
+app.listen(3000, () => {
+  console.log('Servidor corriendo en el puerto 3000');
+});
