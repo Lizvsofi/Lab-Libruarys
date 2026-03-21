67a98d2e587bc9c84511dd021ec103330b3435d6
swenderd <swenderd25@gmail.com>
2026-03-21 11:34:37 -0600

    Actualización

--- backend/src/main/java/com/libruarys/model/Libro.java
+++ backend/src/main/java/com/libruarys/model/Libro.java
@@ -35,7 +35,7 @@ private BigDecimal precio;
     @Column(name = "imagen_url", length = 255)
     private String imagen;
 
-    // --- GETTERS Y SETTERS ---
+    
     public Integer getId() { return id; }
     public void setId(Integer id) { this.id = id; }
 
